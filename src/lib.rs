60fad570cabec80fe0c35324f1aad6c2572238f5
Martin Muñoz <im.mmun@gmail.com>
2016-10-05 23:14:23 -0400

    yay it all works

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,28 @@
+use std::ptr;
+use std::slice;
+use std::io::{Write, BufWriter, Stdout};
+use std::ops::Range;
+use std::str::{self, FromStr};
+
+#[macro_use]
+extern crate nom;
+
+use nom::IResult;
+
 #[macro_use]
 extern crate lazy_static;
 
 extern crate libc;
 
-use std::io::{Write, BufWriter, Stdout};
+use libc::{size_t, c_void};
+
 
 mod sync;
 use sync::NotThreadSafe;
+
+mod scan;
+
+/// Printing
 
 lazy_static! {
     pub static ref STDOUT: NotThreadSafe<BufWriter<Stdout>> = {
@@ -46,4 +62,188 @@ macro_rules! print {
 #[macro_export]
 macro_rules! flush {
     () => { $crate::flush() }
+}
+
+/// Reading and Scanning
+
+const DEFAULT_BUF_SIZE: usize = 64 * 1024;
+static mut STDIN_BUF: [u8; DEFAULT_BUF_SIZE] = [0; DEFAULT_BUF_SIZE];
+
+lazy_static! {
+    pub static ref STDIN: NotThreadSafe<Stdin<'static>> = {
+        NotThreadSafe::new(
+            Stdin::new(unsafe { &mut STDIN_BUF })
+        )
+    };
+}
+
+pub fn stdin() -> &'static mut Stdin<'static> {
+    unsafe { STDIN.get().as_mut().unwrap() }
+}
+
+pub struct Stdin<'a> {
+    buf: &'a mut [u8],
+    win: Range<usize>
+}
+
+impl<'a> Stdin<'a> {
+    fn new(buf: &'a mut [u8]) -> Stdin<'a> {
+        Stdin { buf: buf, win: 0..0 }
+    }
+
+    fn refill(&mut self, keep: usize) {
+        if keep > 0 {
+            // assert!(2*keep <= self.buf.len());
+            unsafe {
+                ptr::copy_nonoverlapping(
+                    self.buf.as_ptr().offset((self.buf.len() - keep) as isize),
+                    self.buf.as_mut_ptr(),
+                    keep
+                );
+            }
+        }
+
+        unsafe {
+            libc::read(
+                libc::STDIN_FILENO,
+                self.buf.as_mut_ptr().offset(keep as isize) as *mut c_void,
+                (self.buf.len() - keep) as size_t
+            );
+        }
+
+        self.win = 0..self.buf.len()
+    }
+
+    fn read_byte(&mut self) -> u8 {
+        if self.win.len() == 0 { self.refill(0); }
+
+        let value = self.buf[self.win.start];
+        self.win.start += 1;
+        value
+    }
+
+    fn read_char(&mut self) -> char {
+        self.read_byte() as char
+    }
+
+    fn scan_float(&mut self) -> &[u8] {
+        self.scan_nom_parser(scan::float::float)
+    }
+
+    fn scan_unsigned_integer(&mut self) -> &[u8] {
+        self.scan_nom_parser(scan::int::unsigned_integer)
+    }
+
+    fn scan_signed_integer(&mut self) -> &[u8] {
+        self.scan_nom_parser(scan::int::signed_integer)
+    }
+
+    fn scan_nom_parser<F>(&mut self, scanner: F) -> &[u8]
+        where F: Fn(&[u8]) -> IResult<&[u8], &[u8]> {
+
+        loop {
+            if self.win.len() == 0 {
+                self.refill(0);
+            }
+
+            match scanner(&self.buf[self.win.clone()]) {
+                IResult::Error(_) => {
+                    self.win.start += 1;
+                    continue;
+                },
+                IResult::Done(_, res) => {
+                    self.win.start += res.len();
+                    return unsafe {
+                        slice::from_raw_parts(res.as_ptr(), res.len())
+                    };
+                },
+                IResult::Incomplete(_) => {
+                    // Fall through the match to workaround lexical lifetimes.
+                }
+            }
+
+            let window_size = self.win.len();
+            self.refill(window_size);
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! readchar {
+    () => { $crate::read_char() }
+}
+
+#[macro_export]
+macro_rules! readbyte {
+    () => { $crate::read_byte() }
+}
+
+pub fn read_char() -> char {
+    stdin().read_char()
+}
+
+pub fn read_byte() -> u8 {
+    stdin().read_byte()
+}
+
+/// Scanning
+
+#[macro_export]
+macro_rules! scan {
+    ($arg:tt) => {
+        $crate::scan::<$arg>()
+    }
+}
+
+pub fn scan<T>() -> T where Stdin<'static>: Scan<T> {
+    stdin().scan()
+}
+
+pub trait Scan<T> {
+    fn scan(&mut self) -> T;
+}
+
+impl Scan<f32> for Stdin<'static> {
+    fn scan(&mut self) -> f32 {
+        scan_bytes(self.scan_float())
+    }
+}
+
+impl Scan<f64> for Stdin<'static> {
+    fn scan(&mut self) -> f64 {
+        scan_bytes(self.scan_float())
+    }
+}
+
+impl Scan<u8> for Stdin<'static> {
+    fn scan(&mut self) -> u8 {
+        scan_bytes(self.scan_unsigned_integer())
+    }
+}
+
+impl Scan<u16> for Stdin<'static> {
+    fn scan(&mut self) -> u16 {
+        scan_bytes(self.scan_unsigned_integer())
+    }
+}
+
+impl Scan<u32> for Stdin<'static> {
+    fn scan(&mut self) -> u32 {
+        scan_bytes(self.scan_unsigned_integer())
+    }
+}
+
+impl Scan<u64> for Stdin<'static> {
+    fn scan(&mut self) -> u64 {
+        scan_bytes(self.scan_unsigned_integer())
+    }
+}
+
+fn scan_bytes<T>(bytes: &[u8]) -> T where T: FromStr {
+    unsafe {
+        match str::from_utf8_unchecked(bytes).parse::<T>() {
+            Ok(value) => return value,
+            _ => unreachable!()
+        }
+    }
 }
---
+++ src/scan/int.rs
@@ -0,0 +1,32 @@
+use nom::digit;
+
+named!(sign, recognize!(one_of!("+-")));
+named!(plus, recognize!(char!('+')));
+named!(dot, recognize!(char!('.')));
+
+named!(pub unsigned_integer, recognize!(
+    chain!(plus? ~ digit, || {})
+));
+
+named!(pub signed_integer, recognize!(
+    chain!(sign? ~ digit, || {})
+));
+
+#[cfg(test)]
+mod tests {
+    use nom::IResult::*;
+    use super::{unsigned_integer, signed_integer};
+
+    #[test]
+    fn it_scans_unsigned_integers() {
+        assert_eq!(unsigned_integer(b"123 "),   Done(&b" "[..], &b"123"[..]));
+        assert_eq!(unsigned_integer(b"+123 "),  Done(&b" "[..], &b"+123"[..]));
+    }
+
+    #[test]
+    fn it_scans_signed_integers() {
+        assert_eq!(signed_integer(b"123 "),     Done(&b" "[..], &b"123"[..]));
+        assert_eq!(signed_integer(b"+123 "),    Done(&b" "[..], &b"+123"[..]));
+        assert_eq!(signed_integer(b"-123 "),    Done(&b" "[..], &b"-123"[..]));
+    }
+}
